5bceda4d8ca166f7767311b7df1f9ea4fc1edd16
jdragonhx <jayden.dhx@gmail.com>
2025-10-26 03:49:31 +0000

    26/10

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,22 @@
+let ballSpeedX = 5;
+let ballX = 300;
+let ballY = 200;
+let ballSize = 50;
 
+function setup(){
+    createCanvas(600, 400);
+    posX = width/ 2;
+    xSpeed = 5;
+    fill('limegreen');
+}
+
+function draw(){
+    background('lightblue')
+    circle(ballX, ballY, ballSize);
+
+    ballX = ballX + ballSpeedX;
+
+    if (ballX - ballSize/2 <= 0 || ballX + ballSize/2 >= width) {
+        ballSpeedX = ballSpeedX * -1;
+    }
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,20 @@
+let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+let yPos;
 
+function setup(){
+    createCanvas(200, 300);
+    // Task: Align the text to the center using textAlign
+    // Task: set font size to 20 using textSize
+    yPos = 100;
+}
+
+function draw() {
+    textSize(20)
+    textAlign(LEFT, TOP)
+    background('black')
+    for(let i = 0; i < pokemons.length; i++) {
+        text(pokemons[i], 50, 80 + i * 30);
+    }
+
+    fill("pink");
+}
